c43a98d5bde3f9c9151fc1d8768656660c3eda47
hmuhamman <hassan.relatient@nxvt.com>
2015-10-28 19:47:51 +0500

    Base
    Initial files uploaded

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/bnb/admin/');
+define('HTTP_CATALOG', 'http://localhost/bnb/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/bnb/admin/');
+define('HTTPS_CATALOG', 'http://localhost/bnb/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/bnb/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/bnb/system/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/bnb/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/bnb/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/bnb/system/config/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/bnb/image/');
+define('DIR_CACHE', 'C:/xampp/htdocs/bnb/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/bnb/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/bnb/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/bnb/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/bnb/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/bnb/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'oc_bnb');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/bnb/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/bnb/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/bnb/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/bnb/system/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/bnb/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/bnb/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/bnb/system/config/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/bnb/image/');
+define('DIR_CACHE', 'C:/xampp/htdocs/bnb/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/bnb/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/bnb/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/bnb/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/bnb/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'oc_bnb');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
